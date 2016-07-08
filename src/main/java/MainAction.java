11af10cc6103d8227734087199a7f6a2b23fabd6
zjt1388 <459404242@qq.com>
2016-07-08 22:13:43 +0800

    测试提交

--- src/main/java/MainAction.java
+++ src/main/java/MainAction.java
@@ -1,6 +1,7 @@
+import javax.servlet.http.HttpServlet;
 import java.lang.System;
 
-public class MainAction{
+public class MainAction extends HttpServlet{
     public static void main(String[] args){
         System.out.print("ccc");
     }
